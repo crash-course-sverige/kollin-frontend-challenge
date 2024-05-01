ab8d3e408954b24905af21c147c8294663140fa3
ElizPN <pankevych.liza@gmail.com>
2024-05-01 19:05:06 +0200

    use Image from next image

--- src/app/page.js
+++ src/app/page.js
@@ -5,11 +5,9 @@ export default function Home() {
 
         <div className="h-screen w-screen flex flex-col items-center justify-center p-24 bg-[#202746]">
 
-            <img
-
-                src="https://d1mgntrf3vaj6d.cloudfront.net/kollin_logo_white.svg"
-                className="w-64"
-            />
+        <Image alt="logo" src="https://d1mgntrf3vaj6d.cloudfront.net/kollin_logo_white.svg"
+                className="w-64" width="200" height="50" priority="1"/>
+           
         </div>
 
     );
