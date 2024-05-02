24f00161845fb3a50efde7acffe3eb335ad97ab8
ElizPN <pankevych.liza@gmail.com>
2024-05-02 10:41:05 +0200

    fix(styling): repalace dynamic class name with condition classes

--- src/app/practice/Header.jsx
+++ src/app/practice/Header.jsx
@@ -7,11 +7,18 @@ export function Header({ excersises, answersResult, lifes, changeQuestion }) {
     return (
         <div className="flex gap-2 mb-20">
             {excersises.map((id) => {
-                const color = answersResult[id] === undefined ? 'gray' :( answersResult[id] ? 'green': 'red')
+                let bgClass;
+                if (answersResult[id] === undefined) {
+                    bgClass = 'bg-custom-gray';
+                } else if (answersResult[id]) {
+                    bgClass = 'bg-custom-green';
+                } else {
+                    bgClass = 'bg-custom-red';
+                }
                 return (
                     <div
                         key={nanoid()}
-                        className={`relative grid select-none items-center rounded-lg bg-${color}-400 px-12 py-2 cursor-pointer`}
+                        className={`relative grid select-none items-center rounded-lg  px-12 py-2 cursor-pointer ${bgClass}`}
                         onClick={() => changeQuestion(id)}
                     ></div>
                 );
