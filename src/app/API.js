27abade106e84f1bb9c91ab398dc4425540ddbf0
Andrea Micheli <amicheli@kth.se>
2024-05-03 01:57:14 +0200

    fetching working

---
+++ src/app/API.js
@@ -0,0 +1,57 @@
+const url =
+  "https://jgsbshesm5advigzznyid7juny.appsync-api.eu-north-1.amazonaws.com/graphql";
+const query = `
+  query GetAssignment($id: ID!) {
+    getAssignment(id: $id) {
+      id
+      difficultyScore
+      questionText
+      solutionText
+      hints
+      answerOptions {
+        id
+        text
+        correct
+      }
+      createdAt
+      updatedAt
+    }
+  }
+`;
+
+async function fetchAPI(ids) {
+  let fetchPromises = ids.map((id) => {
+    const variables = { id: id };
+    const requestOptions = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization:
+          "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6IlhTVVAwa3ZaUHl3S296bkU1SkNGMW1KbnJnT29CdTRjVHBTMDViQWc4RXMifQ.eyJzdWIiOiIyNjQyMSIsImlkIjoyNjQyMSwiZiI6IjlhOTJmNjMxYzNmNjNkZDgzOGNiNzZjZTcwNDZiNmM5IiwibWF4QWxsb3dlZERldmljZXMiOjEsImdyb3VwcyI6WyJQVUJMSUMiXSwiYXVkIjoiaW50ZXJuYWwiLCJleHAiOjE3MTcxMDE0MDgsImlhdCI6MTcxNDUwOTQwOCwiaXNzIjoiaHR0cHM6Ly9hcGkudG50b3Iuc2Uvb2lkYyJ9.QmNBGduFAihbzKd2ETRQ1DukxHta8_G-CRK8RHtLhAqFDcD9pcK6mbdZCRYx-TKG2Ovyi1LS7MpcG-mYNsq8kNrMOHWVgJtDNyJEjgdYQMFZwsfGikKu5KRNHHf1j8g8tYqEcT7Yw_Azv9uMeiGU1CcL1jGRBhbaqVo3G1pXCxVupHbHsKQn237DC7n2fbaiVVM2S2J1bOFSATbfj35yDJmgZzLOQWqGebl4UkfFZcgWImWcj1IwVRogrCWRK5HZbeElgIu02mlcD8XrFpOV1oFgEnMiMmHjdbgPvm_RX4-FkJTJXUXflVRQYhBFVtOH9bf-t1FTY8FM7kV19uRhHw",
+      },
+      body: JSON.stringify({ query, variables }),
+    };
+
+    return fetch(url, requestOptions)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        return data.data.getAssignment;
+      })
+      .catch((error) => {
+        console.error(
+          "There was a problem with the fetch operation:",
+          error.message
+        );
+      });
+  });
+
+  let assignements = await Promise.all(fetchPromises);
+  return assignements;
+}
+
+export default fetchAPI;
--- src/app/page.js
+++ src/app/page.js
@@ -1,16 +1,56 @@
-import Image from "next/image"
+"use client";
+import exercises from "../../exercises.json";
+import { useEffect, useState } from "react";
+import fetchAPI from "./API";
+import Image from "next/image";
 
 export default function Home() {
-    return (
+  const assignmentIDs = exercises;
+  const [isLoading, setIsLoading] = useState(true);
+  const [assignements, setAssignements] = useState([]);
 
-        <div className="h-screen w-screen flex flex-col items-center justify-center p-24 bg-[#202746]">
+  useEffect(() => {
+    const fetchAssignmentsData = async () => {
+      try {
+        let data = await fetchAPI(assignmentIDs);
+        console.log("fetched data:", data);
 
-            <img
+        setAssignements(data);
+        setIsLoading(false);
+      } catch (error) {
+        setIsLoading(null);
+        console.error("Error fetching assignments:", error);
+      }
+    };
 
-                src="https://d1mgntrf3vaj6d.cloudfront.net/kollin_logo_white.svg"
-                className="w-64"
-            />
-        </div>
+    fetchAssignmentsData();
+  }, []);
 
-    );
+  return (
+    <div className="h-screen w-screen flex flex-col items-center justify-center p-24 bg-[#202746]">
+      {isLoading || assignements.length <= 0 ? (
+        <Image
+          src="https://media0.giphy.com/media/CrrOqaQFuhnTdByaYa/giphy.gif?cid=6c09b9527c05r8221e4i2u39tm1kk9fys4699msfr0wb61wq&ep=v1_internal_gif_by_id&rid=giphy.gif&ct=s"
+          alt="loading"
+          width={80}
+          height={80}
+        />
+      ) : (
+        <>{Content()}</>
+      )}
+    </div>
+  );
+
+  function Content() {
+    if (!!assignements && assignements.length > 0)
+      return assignements.map(
+        (assignment) =>
+          !!assignment && (
+            <div key={assignment.id}>
+              <h1>{assignment.questionText}</h1>
+              <p>{assignment.solutionText}</p>
+            </div>
+          )
+      );
+  }
 }
