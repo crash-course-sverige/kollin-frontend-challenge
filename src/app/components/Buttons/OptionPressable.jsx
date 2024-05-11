a29f05b9fe5fa88ef47be04f1691bac33420a97d
Sam Barati Bakhtiiari <sambb@kth.se>
2024-05-11 11:39:23 +0200

    Update option button style

--- src/app/components/Buttons/OptionPressable.jsx
+++ src/app/components/Buttons/OptionPressable.jsx
@@ -5,9 +5,10 @@ export default function OptionPressable({ onClick, active, text }) {
             width: "100%",
             height: 56,
             outline: "solid",
-            outlineWidth: "thin",
-            borderRadius: 2,
-            outlineColor: active ? "ActiveBorder" : "#E7E5E4"
+            outlineWidth: 2,
+            borderRadius: 8,
+            outlineColor: active ? "#A8B9EE" : "#E7E5E4",
+            background: active ? "#E2E8F9":"white"
         }}
             onClick={onClick}>
             {text}
