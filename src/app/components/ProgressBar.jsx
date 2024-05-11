0d455a52a273d50504ac5227e5ee3ae130e800d0
Sam Barati Bakhtiiari <sambb@kth.se>
2024-05-11 11:54:10 +0200

    Update progress bar style

--- src/app/components/ProgressBar.jsx
+++ src/app/components/ProgressBar.jsx
@@ -9,7 +9,9 @@ export default function ProgressBar({ totalItems, current, onClick, correct, att
             background: attempted ? (correct ? "#66C61C" : "#F79009") : "#D7D3D0",
             outline: current || first ? "solid" : "none",
             borderRadius: 30,
-            outlineWidth: "thin",
+            outlineWidth: 3,
+            outlineColor: current ? "#2F80ED": "none",
+            outlineOffset: 2
         }} onClick={onClick} />
 
     )
