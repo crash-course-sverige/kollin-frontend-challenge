a07c12deb1f073d474b4b8b95797787b621283d8
Sam Barati Bakhtiiari <sambb@kth.se>
2024-05-11 11:49:34 +0200

    Update padding

--- src/app/components/Buttons/MainButton.jsx
+++ src/app/components/Buttons/MainButton.jsx
@@ -2,14 +2,13 @@ export default function MainButton({ onClick, prompt }) {
     return (
         <button 
             style={{ 
-                width: "75%", 
+                width: "59%", 
                 height: "48px", 
                 outline: "solid", 
                 outlineWidth: "thin", 
-                borderRadius: 2, 
+                borderRadius: "8px", 
                 background: "#586FB5", 
                 color: "white", 
-                padding: "0 0 20px 0" 
             }} 
             onClick={onClick}
         >
